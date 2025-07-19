44791e1d1fe267fd62769daa75ab5affd8525853
Kumar-demo <kumaranish004321@gmail.com>
2025-07-19 18:41:10 +0530

    new file:   paper.css 	new file:   rock.html 	new file:   scissors.js

---
+++ scissors.js
@@ -0,0 +1,69 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorepara = document.querySelector("#comp-score");
+
+const genCompChoice = () =>{
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+    //rock, paper, scissor
+};
+
+const drawGame = () =>{
+    msg.innerHTML = "Game was draw.Play again";
+    msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) =>{
+    if (userWin) {
+        userScore++;
+        userScorePara.innerHTML = userScore;
+        msg.innerHTML = `You Win! your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else{
+        compScore++;
+        compScorepara.innerHTML = compScore;
+        msg.innerHTML = `You Lost. ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+
+};
+
+const playGame = (userChoice) => {
+   console.log("user Choice = ", userChoice);
+   //Generate computer choice
+   const compChoice= genCompChoice();
+   console.log("comp Choice = ", compChoice);
+
+   if(userChoice=== compChoice) {
+    //Draw Game
+    drawGame();
+   }else{
+    let userWin = true;
+    if(userChoice=== "rock") {
+        //scissors, paper
+        userWin = compChoice==="paper" ? false : true;
+    }else if(userChoice === "paper") {
+       //rock, scissors
+      userWin = compChoice === "scissors" ?  false: true;  
+    }else{
+        //rock, paper
+       userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+   }
+};
+
+
+choices.forEach((choice) =>{
+   
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+}); 
